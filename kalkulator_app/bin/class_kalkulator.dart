80b14770db9a007e235790dec3a3633b9b337907
Fellisaanastasya25 <fellisaanastasya_2428240094@mhs.mdp.ac.id>
2025-09-27 17:04:01 +0700

    Add files via upload

---
+++ kalkulator_app/bin/class_kalkulator.dart
@@ -0,0 +1,13 @@
+class Kalkulator {
+  double tambah(double a, double b) => a + b;
+  double kurang(double a, double b) => a - b;
+  double kali(double a, double b) => a * b;
+
+  double bagi(double a, double b) {
+    if (b == 0) {
+      throw Exception("Tidak dapat melakukan pembagian dengan nol");
+    }
+    return a/b;
+    }
+}
+
